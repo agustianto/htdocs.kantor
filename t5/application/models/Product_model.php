c3d0e0e66749ed28ef1b478455e4c731a9a6be34
Khafidurrohman Agustianto <34321864+agustianto@users.noreply.github.com>
2020-03-05 14:15:49 +0700

    Coba T5

---
+++ t5/application/models/Product_model.php
@@ -0,0 +1,64 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Product_model extends CI_Model
+{
+    private $products = "products";
+
+    public $product_id;
+    public $name;
+    public $price;
+    public $image = "default.jpg";
+    public $description;
+
+    public function rules()
+    {
+        return [
+            ['field' => 'name',
+            'label' => 'Name',
+            'rules' => 'required'],
+
+            ['field' => 'price',
+            'label' => 'Price',
+            'rules' => 'numeric'],
+            
+            ['field' => 'description',
+            'label' => 'Description',
+            'rules' => 'required']
+        ];
+    }
+
+    public function getAll()
+    {
+        return $this->db->get($this->products)->result();
+    }
+    
+    public function getById($id)
+    {
+        return $this->db->get_where($this->products, ["product_id" => $id])->row();
+    }
+
+    public function save()
+    {
+        $post = $this->input->post();
+        $this->product_id = uniqid();
+        $this->name = $post["name"];
+        $this->price = $post["price"];
+        $this->description = $post["description"];
+        $this->db->insert($this->products, $this);
+    }
+
+    public function update()
+    {
+        $post = $this->input->post();
+        $this->product_id = $post["id"];
+        $this->name = $post["name"];
+        $this->price = $post["price"];
+        $this->description = $post["description"];
+        $this->db->update($this->products, $this, array('product_id' => $post['id']));
+    }
+
+    public function delete($id)
+    {
+        return $this->db->delete($this->products, array("product_id" => $id));
+    }
+}
